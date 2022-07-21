95cfa5647a7c9f51821f9133866ab3ab32ec77e7
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-07-21 15:39:19 +0300

    save progress

--- scripts.js
+++ scripts.js
@@ -2,4 +2,12 @@ $(".dive-in-btn").click(function () {
   $("body").css({
     overflow: "visible",
   });
+  setTimeout(() => {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $("#about").offset().top,
+      },
+      2000
+    );
+  }, 10000);
 });
