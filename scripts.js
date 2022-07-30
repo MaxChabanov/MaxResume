ac8c8cfe494cdb30da4213837cbb48603dc3a16e
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-07-30 17:19:59 +0300

    aaaa

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
+let level = 0;
+
 $(".dive-in-btn").click(function () {
   $("body").css({
-    overflow: "visible",
+    overflowY: "visible",
   });
   setTimeout(() => {
     $([document.documentElement, document.body]).animate(
       {
         scrollTop: $("#anchor-about").offset().top,
       },
-      2000
+      2000,
+      function () {
+        $(".arrow-nav").css("opacity", "1");
+      }
     );
+    level = 1;
   }, 1000);
+});
+
+console.log($(".arrow-up"));
+$(".arrow-up").click(function () {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $(document).scrollTop() - 1000,
+    },
+    2000
+  );
+});
+
+$(".arrow-down").click(function () {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $(document).scrollTop() + 1000,
+    },
+    2000
+  );
 });
