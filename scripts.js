010528c07e2a30bab8253bacf0098b1e9001361d
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-08-01 17:19:15 +0300

    some cool animations

--- scripts.js
+++ scripts.js
@@ -4,21 +4,30 @@ $(".dive-in-btn").click(function () {
   $("body").css({
     overflowY: "visible",
   });
+
+  $(".diver").css("animation", "appear 10s 1s forwards");
   setTimeout(() => {
-    $([document.documentElement, document.body]).animate(
-      {
-        scrollTop: $("#anchor-about").offset().top,
-      },
-      2000,
-      function () {
-        $(".arrow-nav").css("left", "0");
-      }
-    );
-    currentSection = 1;
-  }, 1000);
+    $(".diver").css("animation", "dive 3s forwards");
+
+    setTimeout(() => {
+      $([document.documentElement, document.body]).animate(
+        {
+          scrollTop: $("#anchor-about").offset().top,
+        },
+        2000,
+        function () {
+          $(".arrow-nav").css("left", "0");
+        }
+      );
+      currentSection = 1;
+    }, 7000);
+  }, 10000);
 });
 
 $(".arrow-up").click(function () {
+  if (currentSection == 1) {
+    $(".arrow-nav").css("left", "-100%");
+  }
   if (!$("body").is(":animated")) {
     $([document.documentElement, document.body]).animate(
       {
