4c9a81fc45a5de674ab94234d379bdb759463a83
MaxChabanov <84992334+MaxChabanov@users.noreply.github.com>
2022-08-03 12:59:42 +0300

    improved the animatons some more

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 let currentSection = 0; // 0 = header; 1 = about; 2 = projects
+let diverAlreadyAnimated = false;
 
 $(".dive-in-btn").click(function () {
-  $("body").css({
-    overflowY: "visible",
-  });
+  if (diverAlreadyAnimated === false) {
+    $(".diver").css(
+      "animation",
+      "appear 10s 1s, dive 3s 7s, jump 3s 9.55s forwards, swimUp 2s 13.5s forwards"
+    );
 
-  $(".diver").css(
-    "animation",
-    "appear 10s 1s, dive 3s 7s, jump 3s 9.8s forwards, swimUp 2s 13.5s forwards"
-  );
-
-  setTimeout(() => {
+    setTimeout(() => {
+      $([document.documentElement, document.body]).animate(
+        {
+          scrollTop: $("#anchor-about").offset().top,
+        },
+        2000,
+        function () {
+          $(".arrow-nav").css("left", "0");
+        }
+      );
+      currentSection = 1;
+      diverAlreadyAnimated = true;
+    }, 13000);
+  } else {
     $([document.documentElement, document.body]).animate(
       {
         scrollTop: $("#anchor-about").offset().top,
@@ -21,7 +32,7 @@ $(".dive-in-btn").click(function () {
       }
     );
     currentSection = 1;
-  }, 13000);
+  }
 });
 
 $(".arrow-up").click(function () {
